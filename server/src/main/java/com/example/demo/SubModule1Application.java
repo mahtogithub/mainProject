ee2536e6df3db3e1a99b3c0d02685762291b162b
Santosh Mahto <Santosh@IRESLAB.LOCAL>
2018-10-09 14:52:05 +0530

    dddd

---
+++ server/src/main/java/com/example/demo/SubModule1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubModule1Application.class, args);
+		System.out.println("Test submodule code change1..!!");
+		System.out.println("Test submodule code change2..!!");
+	}
+}
